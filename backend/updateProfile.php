3d6ed3276501e7ff7d35f3c74bf4331056b271b4
UniverseBible <34429198+UniverseBible@users.noreply.github.com>
2020-10-17 20:16:23 +0800

    Add files via upload

---
+++ backend/updateProfile.php
@@ -0,0 +1,61 @@
+<?php
+    require 'connectMySQL.php';
+
+    session_start();
+
+    if (isset($_POST["email"]) && isset($_POST["password"])) {
+
+        // Check in DB
+        $db = new MySQLDatabase();
+        $db->connect();
+        $email = $_POST["email"];
+        $name = $_POST["name"];
+        $password = $_POST["password"];
+        $phone = $_POST["phone"];
+		    $extra_info = $_POST["extra_info"];
+		    $name_on_in = $_POST["name_on_in"];
+        $bill_address = $_POST["bill_address"];
+		    $home_address = $_POST["home_address"];
+
+        echo $name;
+        echo $password;
+        echo $phone;
+
+
+
+
+        $query = "SELECT * FROM user WHERE email = '$email'";
+        $result = $db->query($query);
+
+        /*while($row = mysqli_fetch_array($result)) {
+            print "Name: {$row['username']} has ID: {$row['userId']}";
+        }*/
+        if ($row = mysqli_fetch_array($result)) {
+            echo '<script>alert("Email already exist")</script>';
+            // if ($_POST['password'] === $row['Password']) {
+            //     $_SESSION["email"] = $_POST["email"];
+            //     $_SESSION["Username"] = $row['Name'];
+            //     header("Location: Signin.php");
+            //     echo($_POST["email"]);
+        } else {
+            $query2 = "INSERT INTO user (email, password, name, phone_number, extra_information,name_on_invocie ,bill_address,home_address  ) VALUES ('$email','$password','$name','$phone','$extra_info','$name_on_in','$bill_address','$home_address')";
+            if ($db->query($query2) === TRUE) {
+              echo '<script language="javascript" type="text/javascript">
+                  alert("Update succeed!");
+                  window.location = "../web/UserProfile.html";
+                </script>';
+              // echo '<script>alert("Register succeed"); window.location = Signin.php</script>';
+              // header("Location: Signin.php");
+
+              // header("Location: Signin.php");
+
+            } else {
+              echo "Error: " . $sql . "<br>" . $conn->error;
+              }
+
+        }
+        $db->disconnect();
+
+    }
+
+?>
