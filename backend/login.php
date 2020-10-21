e62a093b9a17a87817753ad33340f908ffa01489
Sen7u <32957881+Sen7u@users.noreply.github.com>
2020-10-21 22:56:40 +1100

    update booking

--- backend/login.php
+++ backend/login.php
@@ -12,7 +12,6 @@
             setcookie("passowrd", $_POST["inputPassword"], time() + 60*60*24, "/");
             // $_COOKIE["email"] = $_POST["inputEmail"];
         } else {
-            setcookie("email", "", time() - 60*60*24, "/");
             setcookie("passowrd", $_POST["inputPassword"], time() - 60*60*24, "/");
             // setcookie("inputEmail", null, -1, "/");
         }
