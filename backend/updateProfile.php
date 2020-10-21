a647cbc4b8576f03f837fac284cee8b2e8f5d8ac
Sen7u <32957881+Sen7u@users.noreply.github.com>
2020-10-21 22:05:38 +1100

    update profile homepage

--- backend/updateProfile.php
+++ backend/updateProfile.php
@@ -2,23 +2,25 @@
     require 'connectMySQL.php';
 
     session_start();
-
-    if (isset($_POST["email"]) && isset($_POST["password"])) {
+    if (isset($_POST["name"])) {
+        
 
         // Check in DB
         $db = new MySQLDatabase();
         $db->connect();
         $email = $_SESSION["email"];
-        $password = $_POST["password"];
-        $phone = $_POST["phone_number"];
+        $name = $_POST["name"];
+
+        $phone = $_POST["phonenumber"];
 		    $name_on_in = $_POST["name_on_in"];
         $bill_address = $_POST["bill_address"];
-		    $home_address = $_POST["home_address"];
+		    $home_address = $_POST["address"];
+        $extra_information= $_POST["information"];
 
 
-        if ($_POST["password"] === '******'){
-          $password = $_SESSION["password"];
-        }
+        // if ($_POST["password"] === '******'){
+        //   $password = $_SESSION["password"];
+        // }
 
 
 
@@ -38,18 +40,19 @@
             //     $_SESSION["Username"] = $row['Name'];
             //     header("Location: Signin.php");
             //     echo($_POST["email"]);
-            $query2 = "UPDATE user  SET email='$email', password='$password', phone_number='$phone',name_on_invoice='$name_on_in' ,bill_address='$bill_address',home_address='$home_address'   WHERE email ='$email' ";
+            $query2 = "UPDATE user  SET name='$name', phone_number='$phone',extra_information='$extra_information',name_on_invoice='$name_on_in' ,bill_address='$bill_address',home_address='$home_address'   WHERE email ='$email' ";
             if ($db->query($query2) === TRUE) {
-              $_SESSION["email"] = $email;
-              $_SESSION["password"] = $password;
+              $_SESSION["name"] = $name;
               $_SESSION['logged_in'] = TRUE;
               $_SESSION['home_address'] = $home_address;
               $_SESSION['bill_address'] = $bill_address;
               $_SESSION['phone_number'] = $phone;
               $_SESSION['name_on_in'] = $name_on_in;
+              $_SESSION['extra_information'] = $extra_information;
+
               echo '<script language="javascript" type="text/javascript">
                   alert("Update succeed!");
-                  window.location = "../web/UserProfile.php";
+                  window.location = "../web/UserProfile.html";
                 </script>';
               // echo '<script>alert("Register succeed"); window.location = Signin.php</script>';
               // header("Location: Signin.php");
@@ -69,6 +72,11 @@
         }
         $db->disconnect();
 
+    }else{
+      echo '<script language="javascript" type="text/javascript">
+                  alert("Update fail!");
+                  window.location = "../web/UserProfile.html";
+                </script>';
     }
 
 ?>
