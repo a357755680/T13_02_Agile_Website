8f7588dd3a92e22475358284be9b848b546c95e8
UniverseBible <34429198+UniverseBible@users.noreply.github.com>
2020-10-22 01:00:57 +0800

    Add files via upload

--- backend/updateAppointment.php
+++ backend/updateAppointment.php
@@ -11,17 +11,35 @@
 
         $email = $_SESSION["email"];
         $service_charge = 300 ;
-        $service_id = 0 ;
-        $service_type  = $POST["service_type"];
-        $service_date  = $POST["service_date"];
-        $service_timeslot  = $POST["service_timeslot"];
+        // $service_id = 0 ;
+        $service_type  = $_POST["service_type"];
+        // $service_date  = $POST["service_date"];
+        // $service_timeslot  = $POST["service_timeslot"];
+        $message = $_POST["message"];
+        $home_address = $_POST["home_address"];
+        $service_time = $_POST["service_time"];
+        $service_date = $_POST["service_date"];
 
 
-        echo $name;
-        echo $password;
-        echo $phone;
+        echo $service_type;
+        echo $service_time;
 
 
+        if ($service_time == "9:00 - 10:00 AM"){
+          $service_time = 2;
+        }elseif ($service_time == "10:00 - 11:00 AM") {
+          $service_time = 2;
+        }elseif ($service_time == "11:00 - 12:00 AM") {
+          $service_time = 2;
+        }elseif ($service_time == "1:00 - 2:00 PM") {
+          $service_time = 2;
+        }elseif ($service_time == "2:00 - 3:00 PM") {
+          $service_time = 2;
+        }elseif ($service_time == "3:00 - 4:00 PM") {
+          $service_time = 2;
+        }elseif ($service_time == "4:00 - 5:00 PM") {
+          $service_time = 2;
+        }
 
 
         $query = "SELECT * FROM user WHERE email = '$email'";
@@ -37,7 +55,7 @@
             //     $_SESSION["Username"] = $row['Name'];
             //     header("Location: Signin.php");
             //     echo($_POST["email"]);
-            $query2 = "INSERT INTO user_service (service_id, user_email, service_type, service_charge, service_date, service_timeslot) VALUES ('$service_id','$email','$service_type','$service_charge','$service_date','$service_timeslot')";
+            $query2 = "INSERT INTO user_service (user_email, service_type, service_charge, service_date, service_timeslot, home_adress, message) VALUES ('$email','$service_type','$service_charge','$service_date','$service_time','$home_address','$message')";
             if ($db->query($query2) === TRUE) {
 
               echo '<script language="javascript" type="text/javascript">
